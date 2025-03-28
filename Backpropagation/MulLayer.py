2b9be508e345aa4e0cbacc757da0695190079b82
KaimuSeino <skim07a19yki@icloud.com>
2025-03-28 16:34:22 +0900

    乗算レイヤにコメントを追加

--- Backpropagation/MulLayer.py
+++ Backpropagation/MulLayer.py
@@ -1,12 +1,24 @@
 class MulLayer:
     def __init__(self):
         """
-        順伝播の入力を保持するために使用する
+        初期化メソッド
+        順伝播の際に入力された値を保持するための変数を定義する。
         """
-        self.x = None
-        self.y = None
+        self.x = None # 順伝播で入力される一つ目の値
+        self.y = None # 順伝播で入力される二つ目の値
     
     def forward(self, x, y):
+        """
+        順伝播（forward propagation）
+        2つの入力値を掛け算し、その結果を出力する。
+
+        Parameters:
+        - x: 一つ目の入力値
+        - y: 二つ目の入力値
+
+        Returns:
+        - out: 掛け算の結果
+        """
         self.x = x
         self.y = y
         out = x * y
@@ -14,8 +26,19 @@ class MulLayer:
         return out
     
     def backward(self, dout):
-        dx = dout * self.y # `x`と`y`をひっくり返す
-        dy = dout * self.x
+        """
+        逆伝播（backward propagation）
+        出力側から伝わってきた勾配（dout）に対して、入力値それぞれへの勾配を計算して返す。
+
+        Parameters:
+        - dout: 出力側からの勾配
+
+        Returns:
+        - dx: 入力xに対する勾配（doutに入力値yを掛けたもの）
+        - dy: 入力yに対する勾配（doutに入力値xを掛けたもの）
+        """
+        dx = dout * self.y # xに対する勾配はyを掛ける
+        dy = dout * self.x # yに対する勾配はxを掛ける
 
         return dx, dy
 
