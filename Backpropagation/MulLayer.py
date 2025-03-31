54d81c378e5df82fba138f55a6c4e139bda8b872
KaimuSeino <skim07a19yki@icloud.com>
2025-03-31 18:46:44 +0900

    乗算レイヤのクラスのみ記述するように修正

--- Backpropagation/MulLayer.py
+++ Backpropagation/MulLayer.py
@@ -41,26 +41,3 @@ class MulLayer:
         dy = dout * self.x # yに対する勾配はxを掛ける
 
         return dx, dy
-
-apple = 100
-apple_num = 2
-tax = 1.1
-
-# layer
-mul_apple_layer = MulLayer()
-mul_tax_layer = MulLayer()
-
-# forward
-apple_price = mul_apple_layer.forward(apple, apple_num)
-price = mul_tax_layer.forward(apple_price, tax)
-
-print("合計の値段", price)
-
-# backword
-dprice = 1
-dapple_price, dtax = mul_tax_layer.backward(dprice)
-dapple, dapple_num = mul_apple_layer.backward(dapple_price)
-
-print("リンゴの値段の微分", dapple)
-print("リンゴの個数の微分", dapple_num)
-print("消費税の微分", dtax)
