1adadd42151c749eec093ddf70ff0fb0105e3e2a
KaimuSeino <skim07a19yki@icloud.com>
2025-04-05 10:46:30 +0900

    乗算レイヤのコメント修正

--- Backpropagation/MulLayer.py
+++ Backpropagation/MulLayer.py
@@ -1,23 +1,26 @@
 class MulLayer:
+    """
+    乗算レイヤ : 2つの入力値を掛け算するレイヤー
+    """
     def __init__(self):
         """
-        初期化メソッド
-        順伝播の際に入力された値を保持するための変数を定義する。
+        乗算レイヤの初期化メソッド。
         """
         self.x = None # 順伝播で入力される一つ目の値
         self.y = None # 順伝播で入力される二つ目の値
     
     def forward(self, x, y):
         """
-        順伝播（forward propagation）
-        2つの入力値を掛け算し、その結果を出力する。
+        順伝播（forward propagation） : 2つの入力値を掛け算し、その結果を出力する。
 
-        Parameters:
-        - x: 一つ目の入力値
-        - y: 二つ目の入力値
+        Parameters
+        ----------
+        - x : 一つ目の入力値
+        - y : 二つ目の入力値
 
-        Returns:
-        - out: 掛け算の結果
+        Returns
+        -------
+        - out : 掛け算の結果
         """
         self.x = x
         self.y = y
@@ -27,15 +30,16 @@ class MulLayer:
     
     def backward(self, dout):
         """
-        逆伝播（backward propagation）
-        出力側から伝わってきた勾配（dout）に対して、入力値それぞれへの勾配を計算して返す。
+        逆伝播（backward propagation） : 出力側から伝わってきた勾配（dout）に対して、入力値それぞれへの勾配を計算して返す。
 
-        Parameters:
-        - dout: 出力側からの勾配
+        Parameters
+        ----------
+        - dout : 出力側からの勾配
 
-        Returns:
-        - dx: 入力xに対する勾配（doutに入力値yを掛けたもの）
-        - dy: 入力yに対する勾配（doutに入力値xを掛けたもの）
+        Returns
+        -------
+        - dx : 入力xに対する勾配（doutに入力値yを掛けたもの）
+        - dy : 入力yに対する勾配（doutに入力値xを掛けたもの）
         """
         dx = dout * self.y # xに対する勾配はyを掛ける
         dy = dout * self.x # yに対する勾配はxを掛ける
