2c3603784a438cadf55fcd01752699f4fa3df741
KaimuSeino <skim07a19yki@icloud.com>
2025-04-05 11:30:18 +0900

    ReLUレイヤのコメント修正

--- Backpropagation/ReLULayer.py
+++ Backpropagation/ReLULayer.py
@@ -1,24 +1,24 @@
 class Relu:
+    """
+    ReLU（Rectified Linear Unit）レイヤ : このレイヤは、入力値が0より大きい場合はそのまま出力し、0以下の場合は0を出力。
+    """
     def __init__(self):
-        self.mask = None # 順伝播で使用するマスクを初期化
         """
         ReLU（Rectified Linear Unit）レイヤーの初期化メソッド。
-        このレイヤーは、入力値が0より大きい場合はそのまま出力し、
-        0以下の場合は0を出力する活性化関数を使用します。
         """
+        self.mask = None # 順伝播で使用するマスクを初期化
 
     def forward(self, x):
         """
-        順伝播（forward propagation）
-        入力された値（x）に対してReLUを適用し、その結果を返す。
-        ReLUは、入力値が0より大きい場合はそのまま出力し、
-        0以下の場合は0を出力する活性化関数です。
+        順伝播（forward propagation） : 入力された値（x）に対してReLUを適用し、その結果を返す。
 
-        Parameters:
-        - x: 入力値（NumPy配列）
+        Parameters
+        ----------
+        - x : 入力値（NumPy配列）
 
-        Returns:
-        - out: ReLUを適用した出力値
+        Returns
+        -------
+        - out : ReLUを適用した出力値
         """
         self.mask = (x <= 0) # xが0以下の位置をTrueにするマスクを作成
         out = x.copy() # 入力xのコピーを作成
@@ -28,14 +28,15 @@ class Relu:
 
     def backward(self, dout):
         """
-        逆伝播（backward propagation）
-        順伝播で作成したマスクを使用して、勾配（dout）を計算する。
+        逆伝播（backward propagation） : 順伝播で作成したマスクを使用して、勾配（dout）を計算する。
 
-        Parameters:
-        - dout: 勾配（NumPy配列）
+        Parameters
+        ----------
+        - dout : 勾配（NumPy配列）
         
-        Returns:
-        - dx: ReLUの勾配
+        Returns
+        -------
+        - dx : ReLUの勾配
         """
         dout[self.mask] = 0 # マスクがTrueの位置を0にする
         dx = dout
