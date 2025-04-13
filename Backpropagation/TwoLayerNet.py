42bf6012d112867325dea0c5b10e34d25ef4568a
KaimuSeino <skim07a19yki@icloud.com>
2025-04-13 13:38:24 +0900

    不要な関数を削除

--- Backpropagation/TwoLayerNet.py
+++ Backpropagation/TwoLayerNet.py
@@ -1,33 +1,12 @@
 import numpy as np
+import sys, os
 from collections import OrderedDict
 from Backpropagation.AffineLayer import Affine
 from Backpropagation.ReLULayer import Relu
 from Backpropagation.SoftmaxWithLossLayer import SoftmaxWithLoss
 
-from tqdm import tqdm
-
-def numerical_gradient(f, x):
-    h = 1e-4 # 微小値
-    grad = np.zeros_like(x) # 勾配の初期化
-
-    for idx in tqdm(np.ndindex(x.shape), total=x.size, desc="Calculating gradients"):
-        tmp_val = x[idx]
-
-        # f(x+h)の計算
-        x[idx] = tmp_val + h
-        fxh1 = f(x) # f(x+h)
-
-        # f(x-h)の計算
-        x[idx] = tmp_val - h
-        fxh2 = f(x) # f(x-h)
-
-        # 勾配の計算
-        grad[idx] = (fxh1 - fxh2) / (2 * h)
-
-        # 元の値に戻す
-        x[idx] = tmp_val
-
-    return grad
+sys.path.append(os.pardir)
+from gradient.numerical import numerical_gradient
 
 class TwoLayerNet:
 
