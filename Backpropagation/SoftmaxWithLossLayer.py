7f612edcc6232b786fb11409f0f7aee6c5b95e22
KaimuSeino <skim07a19yki@icloud.com>
2025-04-19 13:53:41 +0900

    softmax関数のコメントを修正

--- Backpropagation/SoftmaxWithLossLayer.py
+++ Backpropagation/SoftmaxWithLossLayer.py
@@ -12,10 +12,10 @@ def softmax(x: np.ndarray):
     -------
     - y : softmax関数を通した出力値（Numpy配列）
     """
-    c = np.max(x) # オーバーフロー対策
-    exp_x = np.exp(x - c) # exp(x)の計算
-    sum_exp_x = np.sum(exp_x) # exp(x)の合計
-    y = exp_x / sum_exp_x # softmaxの計算
+    c = np.max(x) # 入力値の最大値を取得
+    exp_x = np.exp(x - c) # オーバーフロー対策のため、最大値を引く
+    sum_exp_x = np.sum(exp_x)
+    y = exp_x / sum_exp_x
 
     return y
 
