bc2176dc3c2595847c831766698af29543f52868
KaimuSeino <skim07a19yki@icloud.com>
2025-08-02 17:44:30 +0900

    DeepConvNetの学習結果を可視化して、認識精度を確認する

---
+++ cnn/misclassified_mnist.py
@@ -0,0 +1,61 @@
+import os, sys
+import numpy as np
+import matplotlib.pyplot as plt
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from data.mnist import load_mnist
+from network.deep_convnet import DeepConvNet
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
+
+network = DeepConvNet()
+network.load_params("deep_convnet_params.pkl")
+
+print("calculating test accuracy ... ")
+#sampled = 1000
+#x_test = x_test[:sampled]
+#t_test = t_test[:sampled]
+
+classified_ids = []
+
+acc = 0.0
+batch_size = 100
+
+for i in range(int(x_test.shape[0] / batch_size)):
+    tx = x_test[i*batch_size:(i+1)*batch_size]
+    tt = t_test[i*batch_size:(i+1)*batch_size]
+    y = network.predict(tx, train_flg=False)
+    y = np.argmax(y, axis=1)
+    classified_ids.append(y)
+    acc += np.sum(y == tt)
+    
+acc = acc / x_test.shape[0]
+print("test accuracy:" + str(acc))
+
+classified_ids = np.array(classified_ids)
+classified_ids = classified_ids.flatten()
+ 
+max_view = 20
+current_view = 1
+
+fig = plt.figure()
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.2, wspace=0.2)
+
+mis_pairs = {}
+for i, val in enumerate(classified_ids == t_test):
+    if not val:
+        ax = fig.add_subplot(4, 5, current_view, xticks=[], yticks=[])
+        ax.imshow(x_test[i].reshape(28, 28), cmap=plt.cm.gray_r, interpolation='nearest')
+        mis_pairs[current_view] = (t_test[i], classified_ids[i])
+
+        current_view += 1
+        if current_view > max_view:
+            break
+
+print("======= misclassified result =======")
+print("{view index: (label, inference), ...}")
+print(mis_pairs)
+
+plt.show()
